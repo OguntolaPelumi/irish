ab30a9cdcc2638e71cecbaa61cd06b4fd7fbadc0
OldStunna27 <168038090+OldStunna27@users.noreply.github.com>
2025-10-09 05:21:45 -0400

    Updated site domain to irishroversinc.onrender.com

--- irish/settings.py
+++ irish/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-@1qpz2$k@v0yc2(tvppzru($tbt^iwy2+pulx+85mc2$!s0p9i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["irishroverinc.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["irishroversinc.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
